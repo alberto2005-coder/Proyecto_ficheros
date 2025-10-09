3a4165cb35cd575708f053870adb596c4e544d08
lauralguacillenguajedema-admin <ninapriego4@gmail.com>
2025-10-09 17:53:14 +0200

    no message

--- src/main/java/com/example/fileinfo/Aplicacion.java
+++ src/main/java/com/example/fileinfo/Aplicacion.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FileInfoApplication {
+public class Aplicacion {
     public static void main(String[] args) {
-        SpringApplication.run(FileInfoApplication.class, args);
+        SpringApplication.run(Aplicacion.class, args);
     }
 }
